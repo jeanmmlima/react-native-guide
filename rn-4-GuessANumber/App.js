fafc57992e9e770f01e017b93e77acc29429dd29
jeanmmlima <jeanmariomlima@gmail.com>
2022-02-23 13:57:38 -0300

    guessing number part I

--- rn-4-GuessANumber/App.js
+++ rn-4-GuessANumber/App.js
@@ -1,13 +1,38 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
 
 export default function App() {
+
+  const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
+
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+    setGuessRounds(0);
+  };
+
+  const gameOverHandler = numofRounds => {
+    setGuessRounds(numofRounds)
+  }
+
+  let content = <StartGameScreen onStartGame={startGameHandler}/>;
+
+  if(userNumber && guessRounds <= 0){
+    content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler}/>
+  } else if (guessRounds > 0){
+    content = <GameOverScreen />;
+  };
+
+
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number"/>
-      <StartGameScreen />
+      {content}
     </View>
   );
 }
---
+++ rn-4-GuessANumber/screens/GameScreen.js
@@ -0,0 +1,101 @@
+import React, { useState, useRef, useEffect } from "react";
+import { View, Text, StyleSheet, Button, Alert } from 'react-native'
+import NumberContainer from "../components/NumberContainer";
+import Card from "../components/Card";
+
+
+const generateRandomBetween = (min, max, exclude) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    const randNumber = Math.floor(Math.random() * (max - min)) + min;
+    if (randNumber === exclude) {
+        return generateRandomBetween(min, max, exclude);
+    } else {
+        return randNumber;
+    }
+}
+
+const GameScreen = props => {
+
+    const [currentGuess, setCurrentGuess] = useState(
+        generateRandomBetween(1, 100, props.userChoice)
+    );
+
+    const [rounds, setRounds] = useState(0);
+
+    const currentLow = useRef(1);
+    const currentHigh = useRef(100);
+
+    /** 
+     * useEffet tem uma função que é executada após cada ciclo de renderização para esse componente!
+     * Toda vez que é renderizado, a função dentro de useEffect é executada. DEPOIS da renderização
+     * Será usada para avaliar o resultado do jogo
+     * 
+     * Primeiro argumento é uma função 
+     * Segundo argumento: é um vetor de dependencias da função passada, no qual você especifica
+     * qualquer valor proviniente de fora da função
+     * Nesse exemplo: onGameOver e userChoice que vem de props
+     * 
+     * Effect só vai ser executada se uma das dependencias for alterada!!!
+     * */
+    /**
+     * Concentrando as dependencias de props necessárias ao effect
+     * assim, o effect só será executado quando UMA dessas dependencias for alterada
+     * Passado o props direto, iria ser executado para qualquer mudança em props
+     */
+    const {userChoice, onGameOver} = props;
+
+    useEffect(() => {
+        if(currentGuess === userChoice){
+            onGameOver(rounds);
+        }
+    }, [currentGuess,userChoice,onGameOver])
+
+    const nextGuessHandler = direction => {
+        if((direction === 'lower' && currentGuess < props.userChoice) ||
+         (direction === 'greater' && currentGuess > props.userChoice)){
+            Alert.alert("Don\'t lie!", 'You know that this is wrong...', [{text: 'Sorry!', style: 'cancel'}]);
+            return;
+        } else {
+            if(direction === 'lower'){
+                currentHigh.current = currentGuess;
+                
+            } else {
+                currentLow.current = currentGuess;
+            }
+            const nextNumber = generateRandomBetween(currentLow.current,currentHigh.current, currentGuess);
+            setCurrentGuess(nextNumber);
+            setRounds(curRounds => curRounds + 1);
+        }
+    }
+
+    return (
+        <View style={styles.screen}>
+            <Text>Opponent's guess</Text>
+            <NumberContainer>{currentGuess}</NumberContainer>
+            <Card style={styles.buttonContainer}>
+                <Button title="LOWER" onPress={nextGuessHandler.bind(this,'lower')} />
+                <Button title="GREATER" onPress={nextGuessHandler.bind(this, 'greater')} />
+            </Card>
+        </View>
+    );
+
+
+};
+
+const styles = StyleSheet.create({
+    screen:{
+        flex: 1,
+        padding: 10,
+        alignItems: 'center',
+    },
+    buttonContainer: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        marginTop: 20,
+        width: 300,
+        maxWidth: '80%'
+    }
+});
+
+export default GameScreen;
