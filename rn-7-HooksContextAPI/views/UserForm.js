e771e27638d6d1b603ef29d473e9f52b2a6f358f
jeanmmlima <jeanmariomlima@gmail.com>
2022-10-29 16:07:28 -0300

    form de usuarios

--- rn-7-HooksContextAPI/views/UserForm.js
+++ rn-7-HooksContextAPI/views/UserForm.js
@@ -1,9 +1,53 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState } from "react";
+import { Text, View, TextInput, StyleSheet, Button } from "react-native";
 
 //componente funcional
-export default props => {
+export default ({route, navigation}) => {
+    //console.warn(Object.keys(props.route.params)) //acesso ao parametro que foi passado
+
+    //usando o primeiro HOOK para gerencia do estado
+    const [user, setUser] = useState(route.params ? route.params : {})
     return (
-        <Text>User Form</Text>
+        <View style={style.form}>
+            <Text>Nome</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={name => setUser({...user, name})} //operador spreed para pegar todos os atributos do usuário
+                placeholder="Informe o Nome"
+                value={user.name}
+            />
+            <Text>Email</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={email => setUser({...user, email})}
+                placeholder="Informe o E-mail"
+                value={user.email}
+            />
+            <Text>URL do Avatar</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={avatarUrl => setUser({...user, avatarUrl})}
+                placeholder="Informe a URL do Avatar"
+                value={user.avatarUrl}
+            />
+            <Button
+                title="Salvar"
+                onPress={() => {
+                    navigation.goBack()
+                }}
+            />
+        </View>
     );
 }
+
+const style = StyleSheet.create({
+    form: {
+        padding: 12
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 15,
+    }
+})
--- rn-7-HooksContextAPI/views/UserList.js
+++ rn-7-HooksContextAPI/views/UserList.js
@@ -1,13 +1,62 @@
 import React from "react";
-import { FlatList, Text, View } from "react-native";
+import { FlatList, Text, View, StyleSheet, Alert } from "react-native";
+import { ListItem, Avatar, Button, Icon } from "react-native-elements"
 import users from '../data/users'
 
 //componente funcional
 export default props => {
     //console.warn(Object.keys(props))  dados que são passados como atributos para o componente
 
+    function confirmUserDeletion(user) {
+        Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
+            {
+                text: 'Sim',
+                onPress() {
+                    console.warn("delete" + user.id)
+                }
+            },
+            {
+                text: 'Não'
+            }
+        ])
+    }
+
+    function getActions(user) {
+        return (
+            <>
+                <Button
+                    onPress={() => props.navigation.navigate('UserForm', user)}
+                    icon={<Icon name="edit" size={25} color="orange" />}
+                    buttonStyle={{ minHeight: '100%', minWidth: '50%', backgroundColor: 'light-gray' }}
+                />
+                <Button
+                    onPress={() => confirmUserDeletion(user)}
+                    icon={<Icon name="delete" size={25} color="red" />}
+                    buttonStyle={{ minHeight: '100%', minWidth: '50%', backgroundColor: 'gray' }}
+                />
+            </>
+        )
+    }
+
+
     function getUserItem({ item: user }){
-        return <Text>{user.name} - {user.email}</Text>
+        return (
+            <ListItem.Swipeable 
+            key={user.id}
+            bottomDivider
+            rightContent={getActions(user)}
+            rightStyle={style.buttonContainer}
+            onPress={() => props.navigation.navigate('UserForm', user)}
+                
+            >
+                <Avatar rounded source={{ uri: user.avatarUrl }} />    
+                <ListItem.Content>
+                    <ListItem.Title>{user.name}</ListItem.Title>
+                    <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem.Swipeable>
+
+        )
     }
 
     return (
@@ -22,3 +71,9 @@ export default props => {
         </View>
     );
 }
+
+const style = StyleSheet.create({
+    buttonContainer: {
+        flexDirection: 'row'
+    },
+})
